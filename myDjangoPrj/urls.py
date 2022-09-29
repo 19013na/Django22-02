bd41335638c4f3131ff3bb3540e909d74174c3bf
19013na <19013@naver.com>
2022-09-29 14:18:35 +0900

    FBV로 대문/상세 페이지 만들기

--- myDjangoPrj/urls.py
+++ myDjangoPrj/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [ #IP주소/
     path("admin/", admin.site.urls), # IP주소/admin
-    path('blog/', include('blog.urls'))    # IP주소/blog
-    #blog밑에 있는 urls를 부르겠다. but, blog안에 urls가 없음. 만들자
+    path('blog/', include('blog.urls')),    # IP주소/blog
+    # blog밑에 있는 urls를 부르겠다. but, blog안에 urls가 없음. 만들자
+    path('', include('single_pages.urls'))  # IP주소/
 ]
